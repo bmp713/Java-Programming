014c25b7155ea1f4c46ffd2d38b17e81ad29ebee
Brandon Michael Piper <bmp713@yahoo.com>
2018-07-16 22:22:17 -0700

    Update TGraphics.java

--- TGraphics.java
+++ TGraphics.java
@@ -7,28 +7,18 @@
 interface TGraphics {
     /**
        Draw a line from (x1, y1) to (x2, y2).
-       @param x1 - x coordinate of first point.
-       @param y1 - y coordinate of first point.
-       @param x2 - x coordinate of second point.
-       @param y2 - y coordinate of second point.
      */
     void drawLine(int x1, int y1, int x2, int y2);
 
     /**
        Draw a horizontal line of the specified length, with (x1, y1) 
        as the left end.
-       @param x1 - x coordinate of left end of the line.
-       @param y1 - y coordinate of left end of the line.
-       @param length - the length of the line.
      */
     void drawHorizontalLine(int x1, int y1, int length);
 
     /**
        Draw a vertical line of the specified length, with (x1, y1) 
        as the upper end.
-       @param x1 - x coordinate of upper end of the line.
-       @param y1 - y coordinate of upper end of the line.
-       @param length - the length of the line.
      */
     void drawVerticalLine(int x1, int y1, int length);
 
